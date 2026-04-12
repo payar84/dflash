4c3b6ada0e458d2b9a909683c2e36752e27bb989
Payar Ahmadi <payar84@users.noreply.github.com>
2026-04-12 08:49:51 +0800

    Add DraftModel alias for DFlashDraftModel for convenience

--- dflash/__init__.py
+++ dflash/__init__.py
@@ -1,3 +1,6 @@
 # Personal fork of z-lab/dflash
 # Exposing additional utilities for easier top-level access
 from .model import DFlashDraftModel, load_and_process_dataset, sample, extract_context_feature
+
+# Convenience alias - I always forget the full class name
+DraftModel = DFlashDraftModel
